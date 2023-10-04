afb71250fa54e6dcf049e6aad797996cd9f55d10
AdityaPrasanth25 <93645871+AdityaPrasanth25@users.noreply.github.com>
2023-10-04 16:48:33 +0530

    second commit

--- client3.py
+++ client3.py
@@ -46,6 +46,23 @@ def getRatio(price_a, price_b):
         return
     return price_a/price_b
 
+def test_getDataPoint_calculatePrice(self):
+    quotes = [
+        {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2023-10-04 16:34:30.562541', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+        {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2023-10-04 16:34:30.562541', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+
+    for quote in quotes:
+        self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
+
+def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+    quotes = [
+        {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2023-10-04 16:34:30.562541', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+        {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2023-10-04 16:34:30.562541', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+
+    for quote in quotes:
+        self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
 # Main
 if __name__ == "__main__":
